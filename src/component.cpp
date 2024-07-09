b04b4bb041af3d375d6dd95bff00a5d3885e725c
creeper5820 <cqq1960180796@163.com>
2024-07-09 13:12:15 +0000

    update msg & complete component construction

--- src/component.cpp
+++ src/component.cpp
@@ -12,6 +12,41 @@ public:
     Component()
         : MapNode()
     {
+        // input from referee system
+        register_input("/referee/friends/hero/hp", friends_hero_hp_);
+        register_input("/referee/friends/engineer/hp", friends_engineer_hp_);
+        register_input("/referee/friends/infantry_iii/hp", friends_infantry_iii_hp_);
+        register_input("/referee/friends/infantry_iv/hp", friends_infantry_iv_hp_);
+        register_input("/referee/friends/infantry_v/hp", friends_infantry_v_hp_);
+        register_input("/referee/friends/sentry/hp", friends_sentry_hp_);
+        register_input("/referee/friends/outpost/hp", friends_outpost_hp_);
+        register_input("/referee/friends/base/hp", friends_base_hp_);
+
+        register_input("/referee/friends/hero/position", friends_hero_pose_);
+        register_input("/referee/friends/engineer/position", friends_engineer_pose_);
+        register_input("/referee/friends/infantry_iii/position", friends_infantry_iii_pose_);
+        register_input("/referee/friends/infantry_iv/position", friends_infantry_iv_pose_);
+        register_input("/referee/friends/infantry_v/position", friends_infantry_v_pose_);
+        register_input("/referee/friends/sentry/position", friends_sentry_pose_);
+
+        register_input("/referee/enemies/hero/hp", enemies_hero_hp_);
+        register_input("/referee/enemies/engineer/hp", enemies_engineer_hp_);
+        register_input("/referee/enemies/infantry_iii/hp", enemies_infantry_iii_hp_);
+        register_input("/referee/enemies/infantry_iv/hp", enemies_infantry_iv_hp_);
+        register_input("/referee/enemies/infantry_v/hp", enemies_infantry_v_hp_);
+        register_input("/referee/enemies/sentry/hp", enemies_sentry_hp_);
+        register_input("/referee/enemies/outpost/hp", enemies_outpost_hp_);
+        register_input("/referee/enemies/base/hp", enemies_base_hp_);
+
+        register_input("/referee/shooter/bullet_allowance", bullet_);
+
+        // input from auto aim system
+        register_input("/referee/enemies/hero/position", enemies_hero_pose_);
+        register_input("/referee/enemies/engineer/position", enemies_engineer_pose_);
+        register_input("/referee/enemies/infantry_iii/position", enemies_infantry_iii_pose_);
+        register_input("/referee/enemies/infantry_iv/position", enemies_infantry_iv_pose_);
+        register_input("/referee/enemies/infantry_v/position", enemies_infantry_v_pose_);
+        register_input("/referee/enemies/sentry/position", enemies_sentry_pose_);
     }
 
     void update() override
@@ -29,42 +64,81 @@ public:
     }
 
 private:
-    InputInterface<std::array<uint8_t, 7>> friends_hp_;
-    InputInterface<std::array<uint8_t, 7>> enemies_hp_;
+    InputInterface<uint16_t> friends_hero_hp_;
+    InputInterface<uint16_t> friends_engineer_hp_;
+    InputInterface<uint16_t> friends_infantry_iii_hp_;
+    InputInterface<uint16_t> friends_infantry_iv_hp_;
+    InputInterface<uint16_t> friends_infantry_v_hp_;
+    InputInterface<uint16_t> friends_sentry_hp_;
+    InputInterface<uint16_t> friends_outpost_hp_;
+    InputInterface<uint16_t> friends_base_hp_;
 
-    InputInterface<std::array<Eigen::Vector2d, 7>> friends_pose_;
-    InputInterface<std::array<Eigen::Vector2d, 7>> enemies_pose_;
+    InputInterface<Eigen::Vector2d> friends_hero_pose_;
+    InputInterface<Eigen::Vector2d> friends_engineer_pose_;
+    InputInterface<Eigen::Vector2d> friends_infantry_iii_pose_;
+    InputInterface<Eigen::Vector2d> friends_infantry_iv_pose_;
+    InputInterface<Eigen::Vector2d> friends_infantry_v_pose_;
+    InputInterface<Eigen::Vector2d> friends_sentry_pose_;
 
-    InputInterface<uint16_t> base_friend_hp_;
-    InputInterface<uint16_t> outpost_friend_hp_;
+    InputInterface<uint16_t> enemies_hero_hp_;
+    InputInterface<uint16_t> enemies_engineer_hp_;
+    InputInterface<uint16_t> enemies_infantry_iii_hp_;
+    InputInterface<uint16_t> enemies_infantry_iv_hp_;
+    InputInterface<uint16_t> enemies_infantry_v_hp_;
+    InputInterface<uint16_t> enemies_sentry_hp_;
+    InputInterface<uint16_t> enemies_outpost_hp_;
+    InputInterface<uint16_t> enemies_base_hp_;
 
-    InputInterface<uint16_t> base_enemy_hp_;
-    InputInterface<uint16_t> outpost_enemy_hp_;
+    InputInterface<Eigen::Vector2d> enemies_hero_pose_;
+    InputInterface<Eigen::Vector2d> enemies_engineer_pose_;
+    InputInterface<Eigen::Vector2d> enemies_infantry_iii_pose_;
+    InputInterface<Eigen::Vector2d> enemies_infantry_iv_pose_;
+    InputInterface<Eigen::Vector2d> enemies_infantry_v_pose_;
+    InputInterface<Eigen::Vector2d> enemies_sentry_pose_;
 
     InputInterface<uint16_t> bullet_;
 
     void collect_referee_message(rmcs_map::msg::GameStatus& status)
     {
         // the hp of friends
-        for (int index = 0; index < friends_hp_->size(); index++) {
-            status.friends[index].hp = (*friends_hp_)[index];
-        }
+        status.friends_hero.hp         = *friends_hero_hp_;
+        status.friends_engineer.hp     = *friends_engineer_hp_;
+        status.friends_infantry_iii.hp = *friends_infantry_iii_hp_;
+        status.friends_infantry_iv.hp  = *friends_infantry_iv_hp_;
+        status.friends_infantry_v.hp   = *friends_infantry_v_hp_;
+        status.friends_sentry.hp       = *friends_sentry_hp_;
+        status.friends_outpost_hp      = *friends_outpost_hp_;
+        status.friends_base_hp         = *friends_base_hp_;
+
         // the hp of enemies
-        for (int index = 0; index < enemies_hp_->size(); index++) {
-            status.enemies[index].hp = (*enemies_hp_)[index];
-        }
+        status.enemies_hero.hp         = *enemies_hero_hp_;
+        status.enemies_engineer.hp     = *enemies_engineer_hp_;
+        status.enemies_infantry_iii.hp = *enemies_infantry_iii_hp_;
+        status.enemies_infantry_iv.hp  = *enemies_infantry_iv_hp_;
+        status.enemies_infantry_v.hp   = *enemies_infantry_v_hp_;
+        status.enemies_sentry.hp       = *enemies_sentry_hp_;
+        status.enemies_outpost_hp      = *enemies_outpost_hp_;
+        status.enemies_base_hp         = *enemies_base_hp_;
 
         // the hp of base and outpost
-        status.base_friend_hp    = *base_friend_hp_;
-        status.base_enemy_hp     = *base_enemy_hp_;
-        status.outpost_friend_hp = *outpost_friend_hp_;
-        status.outpost_enemy_hp  = *outpost_enemy_hp_;
+        status.friends_base_hp    = *friends_base_hp_;
+        status.friends_outpost_hp = *friends_outpost_hp_;
+        status.enemies_base_hp    = *enemies_base_hp_;
+        status.enemies_outpost_hp = *enemies_outpost_hp_;
 
         // the pose of friends
-        for (int index = 0; index < friends_pose_->size(); index++) {
-            status.friends[0].position.x = (*friends_pose_)[index].x();
-            status.friends[0].position.y = (*friends_pose_)[index].y();
-        }
+        status.friends_hero.pose.x         = (*friends_hero_pose_).x();
+        status.friends_hero.pose.y         = (*friends_hero_pose_).y();
+        status.friends_engineer.pose.x     = (*friends_engineer_pose_).x();
+        status.friends_engineer.pose.y     = (*friends_engineer_pose_).y();
+        status.friends_infantry_iii.pose.x = (*friends_infantry_iii_pose_).x();
+        status.friends_infantry_iii.pose.y = (*friends_infantry_iii_pose_).y();
+        status.friends_infantry_iv.pose.x  = (*friends_infantry_iv_pose_).x();
+        status.friends_infantry_iv.pose.y  = (*friends_infantry_iv_pose_).y();
+        status.friends_infantry_v.pose.x   = (*friends_infantry_v_pose_).x();
+        status.friends_infantry_v.pose.y   = (*friends_infantry_v_pose_).y();
+        status.friends_sentry.pose.x       = (*friends_sentry_pose_).x();
+        status.friends_sentry.pose.y       = (*friends_sentry_pose_).y();
 
         // the bullet of self
         status.bullet = *bullet_;
@@ -73,10 +147,18 @@ private:
     void collect_auto_aim_message(rmcs_map::msg::GameStatus& status)
     {
         // the pose of enemies
-        for (int index = 0; index < enemies_pose_->size(); index++) {
-            status.enemies[0].position.x = (*enemies_pose_)[index].x();
-            status.enemies[0].position.y = (*enemies_pose_)[index].y();
-        }
+        status.enemies_hero.pose.x         = (*enemies_hero_pose_).x();
+        status.enemies_hero.pose.y         = (*enemies_hero_pose_).y();
+        status.enemies_engineer.pose.x     = (*enemies_engineer_pose_).x();
+        status.enemies_engineer.pose.y     = (*enemies_engineer_pose_).y();
+        status.enemies_infantry_iii.pose.x = (*enemies_infantry_iii_pose_).x();
+        status.enemies_infantry_iii.pose.y = (*enemies_infantry_iii_pose_).y();
+        status.enemies_infantry_iv.pose.x  = (*enemies_infantry_iv_pose_).x();
+        status.enemies_infantry_iv.pose.y  = (*enemies_infantry_iv_pose_).y();
+        status.enemies_infantry_v.pose.x   = (*enemies_infantry_v_pose_).x();
+        status.enemies_infantry_v.pose.y   = (*enemies_infantry_v_pose_).y();
+        status.enemies_sentry.pose.x       = (*enemies_sentry_pose_).x();
+        status.enemies_sentry.pose.y       = (*enemies_sentry_pose_).y();
     }
 };
 
--- src/ros2/node.hpp
+++ src/ros2/node.hpp
@@ -21,8 +21,9 @@
 
 class MapNode : public rclcpp::Node {
 public:
-    MapNode();
+    explicit MapNode();
 
+    // @brief publish the games status to ros2 space
     void publish_status(const rmcs_map::msg::GameStatus&);
 
 private:
@@ -30,10 +31,6 @@ private:
     std::shared_ptr<rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>> cost_map_publisher_;
     std::shared_ptr<rclcpp::Publisher<rmcs_map::msg::GameStatus>> status_publisher_;
     std::shared_ptr<rclcpp::Publisher<sensor_msgs::msg::PointCloud2>> cloud_publisher_;
-
-    std::shared_ptr<rclcpp::Subscription<geometry_msgs::msg::Pose2D>> velocity_subscription_;
-    std::shared_ptr<rclcpp::Subscription<std_msgs::msg::Int32>> rotation_subscription_;
-    std::shared_ptr<rclcpp::Subscription<geometry_msgs::msg::Vector3>> gimbal_subscription_;
 
     std::shared_ptr<rclcpp::Subscription<livox_ros_driver2::msg::CustomMsg>> livox_subscription_;
     std::shared_ptr<rclcpp::Subscription<sensor_msgs::msg::PointCloud2>> pointcloud_subscription_;
@@ -43,8 +40,17 @@ private:
     std::shared_ptr<Process> process_;
 
 private:
+    // @brief publish the transform between lidar link and map link
+    // @note lidar: name.frame.lidar
+    // @note map: name.frame.map
     void publish_static_transform();
+
+    // @brief handle the pcl type pointcloud
     void pointcloud_process(const std::shared_ptr<pcl::PointCloud<pcl::PointXYZ>>& pointcloud, const std_msgs::msg::Header& header);
+
+    // @brief work as the livox subscription callback, transform the message to pcl pointcloud
     void livox_subscription_callback(const std::unique_ptr<livox_ros_driver2::msg::CustomMsg>& msg);
+
+    // @brief work as the standard pointcloud2 subscription callback, transform the message to pcl pointcloud
     void pointcloud2_subscription_callback(const std::unique_ptr<sensor_msgs::msg::PointCloud2>& msg);
 };
